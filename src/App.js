540f654ba0ac0767b766a4c09b83ab644bd04a84
Allanetizen <allankosgei747@gmail.com>
2022-06-08 09:39:25 +0300

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import { BrowserRouter as Router} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Router>
       <Navbar/>
+
+      </Router>
       
     </div>
   );
